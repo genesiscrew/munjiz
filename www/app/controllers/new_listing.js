f512b5038e085f3e06f116d86c927d2205f9b036
Adam Wareing <adamwareing241@gmail.com>
2017-03-23 20:33:38 +1300

    Tided and re-designed New-listing

--- www/app/controllers/new_listing.js
+++ www/app/controllers/new_listing.js
@@ -17,48 +17,45 @@ define([
       function ($scope, userService, $ionicHistory, $rootScope, $state, $ionicActionSheet, $ionicPlatform, $cordovaCamera) {
 
         $ionicPlatform.ready(function() {
-
         });
 
+        // Start loading
         $scope.ready = false;
         $scope.listing = {};
 
+        // Wait until the title and price are ready then highlight the button 
         $scope.$watchGroup(['listing.title', 'listing.desc', 'listing.price'], function (newVal) {
           var title = newVal[0] !== undefined,
-          desc = newVal[1] !== undefined,
           price = newVal[2] !== undefined;
           
-          // check all are valid inputs
-          if(title && desc && price){
+          // Check all are valid inputs
+          if(title && price){
             $scope.ready = true;
           }else{
             $scope.ready = false;
           }
+
         });
 
-
+        // Create the listing and upload it to Parse, then go to the listing page
         $scope.createListing = function(){
-
-          if(!$scope.ready){
-            alert("Please ensure you have filled out all the fields");
-          }else{
-
-            console.log(Parse.User.current());
-            var Listing = Parse.Object.extend("Listings");
-            var newListing = new Listing();
-            newListing.set("title", $scope.listing.title);
-            newListing.set("desc", $scope.listing.desc);
-            newListing.set("price", $scope.listing.price);
-            newListing.set("show", true);
-            newListing.set("parent", Parse.User.current());
-            newListing.set("image", $scope.imageURI);
-            newListing.save();
-          }
+          var Listing = Parse.Object.extend("Listings");
+          var newListing = new Listing();
+          newListing.set("title", $scope.listing.title);
+          newListing.set("desc", $scope.listing.desc);
+          newListing.set("price", $scope.listing.price);
+          newListing.set("show", true);
+          newListing.set("parent", Parse.User.current());
+          newListing.set("image", $scope.imageURI);
+          newListing.save();
+          
+          // Go back to listings
           var objectId = Parse.User.current().id;
           $state.go("listing", {id:objectId});
         };
 
-
+        // Trigged by a button click to add an image to a listing. Opens a sheet and displays the option of taking a pboto
+        // or choosing one from the gallery. Then it calls $scope.getPhoto()
         $scope.addMedia = function() {
           $scope.hideSheet = $ionicActionSheet.show({
             buttons: [
@@ -73,8 +70,10 @@ define([
           });
         };
 
+
         // index = 0 = photo lib
         // index = 1 = camera
+        // Get the image URI from the data, and set it to be $scope.imageURI
         $scope.getPhoto = function(index) {
           console.log(index);
           var options = {
